39b145f855cc70f211f2fa5d6bc16cc15c780c01
Shalini Sinha <122859073+Shalinisinha22@users.noreply.github.com>
2025-09-05 11:53:37 +0530

    Update apiClient.ts

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://ewa-back.vercel.app/api';
 
 // Test connectivity to the API
 export const testConnectivity = async () => {
